eb2e7ceaa8d01c69035811298ba6d8bbbd4cec08
Nathaniel michael <153161694+xstealthx13@users.noreply.github.com>
2026-06-03 14:02:23 +0800

    Add files via upload

--- ALP-MAD-26/ContentView.swift
+++ ALP-MAD-26/ContentView.swift
@@ -8,17 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var vm = TimerViewModel(duration: 300)
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        VStack(spacing: 12) {
+                    ProgressView(
+                        value: vm.timerModel.duration - vm.timeRemaining,
+                        total: vm.timerModel.duration
+                    )
+                    .progressViewStyle(.circular)
+                    .scaleEffect(1.3)
+                    Text(formatTime(vm.timeRemaining))
+                        .font(.system(size: 28, weight: .bold))
+                        .monospacedDigit()
+                    HStack {
+                        Button {
+                            vm.startTimer()
+                        } label: {
+                            Image(systemName: "play.fill")
+                        }
+                        .tint(.green)
+                        Button {
+                            vm.stopTimer()
+                        } label: {
+                            Image(systemName: "stop.fill")
+                        }
+                        .tint(.red)
+                    }
+                    .buttonStyle(.bordered)
+                }
+                .padding()
+            }
+            private func formatTime(_ time: TimeInterval) -> String {
+                let minutes = Int(time) / 60
+                let seconds = Int(time) % 60
+                return String(format: "%02d:%02d", minutes, seconds)
+            }
     }
-}
-
 #Preview {
     ContentView()
 }
